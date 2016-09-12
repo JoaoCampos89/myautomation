896e010a29a4c27afebde211e7af38328d494d79
Mariama Saskya <Mariama Saskya>
2016-09-11 21:33:44 -0300

    adding heroku cloud mqtt

--- imports/api/mqtt/settings.js
+++ imports/api/mqtt/settings.js
@@ -1,11 +1,16 @@
 
-
-const mqttSettings = {
+// cloud mqtt
+/*const mqttSettings = {
               server: "m11.cloudmqtt.com",
               user:"jzpzxggs",
               password:"Obn820QGYTqL",
               port: "16330"
+};*/
+// heroku cloud  mqtt settings
+const mqttSettings = {
+              server: "m11.cloudmqtt.com",
+              user:"xopifntw",
+              password:"zuoKBdsfbglH",
+              port: "12359"
 };
-
-
-export default mqttSettings; 
+export default mqttSettings;
